a7b2de3dba49419699e287e92408384eb58d24f4
Shoki52 <halhabai.shokan@gmail.com>
2023-02-26 14:10:54 +0600

    ...

---
+++ cmd/api/books.go
@@ -0,0 +1,238 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"greenlight.sabn.kz/internal/data"
+	"greenlight.sabn.kz/internal/validator"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) createBookHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string       `json:"title"`
+		Sales   int32        `json:"sales"`
+		Pages   int32        `json:"pages"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	// Note that the book variable contains a *pointer* to a Books struct.
+	book := &data.Books{
+		Title:   input.Title,
+		Sales:   input.Sales,
+		Pages:   input.Pages,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	v := validator.New()
+	if data.ValidateBook(v, book); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	// Call the Insert() method on our movies model, passing in a pointer to the
+	// validated book struct. This will create a record in the database and update the
+	// book struct with the system-generated information.
+	err = app.models.Books.Insert(book)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+	// When sending a HTTP response, we want to include a Location header to let the
+	// client know which URL they can find the newly-created resource at. We make an
+	// empty http.Header map and then use the Set() method to add a new Location header,
+	// interpolating the system-generated ID for our new book in the URL.
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/books/%d", book.ID))
+	// Write a JSON response with a 201 Created status code, the book data in the
+	// response body, and the Location header.
+	err = app.writeJSON(w, http.StatusCreated, envelope{"book": book}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) showBookHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	// Call the Get() method to fetch the data for a specific book. We also need to
+	// use the errors.Is() function to check if it returns a data.ErrRecordNotFound
+	// error, in which case we send a 404 Not Found response to the client.
+	book, err := app.models.Books.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"book": book}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) updateBookHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the book ID from the URL.
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	// Fetch the existing book record from the database, sending a 404 Not Found
+	// response to the client if we couldn't find a matching record.
+	book, err := app.models.Books.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	if r.Header.Get("X-Expected-Version") != "" {
+		if strconv.FormatInt(int64(book.Version), 32) != r.Header.Get("X-Expected-Version") {
+			app.editConflictResponse(w, r)
+			return
+		}
+	}
+
+	// Declare an input struct to hold the expected data from the client.
+	var input struct {
+		Title   *string       `json:"title"`
+		Sales   *int32        `json:"sales"`
+		Pages   *int32        `json:"pages"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
+	}
+	// Read the JSON request body data into the input struct.
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	if input.Title != nil {
+		book.Title = *input.Title
+	}
+	if input.Sales != nil {
+		book.Sales = *input.Sales
+	}
+	if input.Pages != nil {
+		book.Pages = *input.Pages
+	}
+	// We also do the same for the other fields in the input struct.
+	if input.Year != nil {
+		book.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		book.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		book.Genres = input.Genres // Note that we don't need to dereference a slice.
+	}
+	// Validate the updated book record, sending the client a 422 Unprocessable Entity
+	// response if any checks fail.
+	v := validator.New()
+	if data.ValidateBook(v, book); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	// Pass the updated book record to our new Update() method.
+	err = app.models.Books.Update(book)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"book": book}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
+
+func (app *application) deleteBookHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the movie ID from the URL.
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	// Delete the movie from the database, sending a 404 Not Found response to the
+	// client if there isn't a matching record.
+	err = app.models.Books.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Return a 200 OK status code along with a success message.
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "book successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) listBooksHandler(w http.ResponseWriter, r *http.Request) {
+	// Embed the new Filters struct.
+	var input struct {
+		Title  string
+		Sales  int32
+		Pages  int32
+		Genres []string
+		data.Filters
+	}
+	v := validator.New()
+	qs := r.URL.Query()
+	input.Title = app.readString(qs, "title", "")
+	input.Genres = app.readCSV(qs, "genres", []string{})
+	// Read the page and page_size query string values into the embedded struct.
+	input.Filters.Sales = app.readInt(qs, "sales", 0, v)
+	input.Filters.Page = app.readInt(qs, "page", 1, v)
+	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
+	// Read the sort query string value into the embedded struct.
+	input.Filters.Sort = app.readString(qs, "sort", "id")
+
+	input.Filters.SortSafelist = []string{"id", "title", "sales", "pages", "year", "runtime", "-id", "-sales", "-pages", "-title", "-year", "-runtime"}
+	// Execute the validation checks on the Filters struct and send a response
+	// containing the errors if necessary.
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	movies, metadata, err := app.models.Books.GetAll(input.Title, input.Sales, input.Pages, input.Genres, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies, "metadata": metadata}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
+}
---
+++ internal/data/books.go
@@ -0,0 +1,240 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"greenlight.sabn.kz/internal/validator"
+	"time"
+)
+
+type Books struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Sales     int32     `json:"sales"`
+	Pages     int32     `json:"pages"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   Runtime   `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
+}
+
+func ValidateBook(v *validator.Validator, book *Books) {
+	v.Check(book.Title != "", "title", "must be provided")
+	v.Check(len(book.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(book.Sales >= 0, "sales", "must be a positive integer")
+	v.Check(book.Pages >= 0, "pages", "must be a positive integer")
+	v.Check(book.Year != 0, "year", "must be provided")
+	v.Check(book.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(book.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(book.Runtime != 0, "runtime", "must be provided")
+	v.Check(book.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(book.Genres != nil, "genres", "must be provided")
+	v.Check(len(book.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(book.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(book.Genres), "genres", "must not contain duplicate values")
+	v.Check(book.Sales >= 0, "sales", "must positive")
+	v.Check(book.Pages >= 0, "pages", "must be positive")
+	v.Check(book.Pages <= 10000, "pages", "must be less than 10000")
+}
+
+type BookModel struct {
+	DB *pgxpool.Pool
+}
+
+func (m BookModel) Insert(books *Books) error {
+
+	query := `
+INSERT INTO books (title, sales, pages, year, runtime, genres)
+VALUES ($1, $2, $3, $4, $5, $6)
+RETURNING id, created_at, version`
+
+	args := []any{books.Title, books.Sales, books.Pages, books.Year, books.Runtime, books.Genres}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	return m.DB.QueryRow(ctx, query, args...).Scan(&books.ID, &books.CreatedAt, &books.Version)
+}
+
+func (m BookModel) Get(id int64) (*Books, error) {
+	// The PostgreSQL bigserial type that we're using for the book ID starts
+	// auto-incrementing at 1 by default, so we know that no movies will have ID values
+	// less than that. To avoid making an unnecessary database call, we take a shortcut
+	// and return an ErrRecordNotFound error straight away.
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+	// Define the SQL query for retrieving the book data.
+	query := `
+SELECT id, created_at, title, sales, pages, year, runtime, genres, version
+FROM books
+WHERE id = $1`
+	// Declare a Books struct to hold the data returned by the query.
+	var book Books
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	// Importantly, use defer to make sure that we cancel the context before the Get()
+	// method returns.
+	defer cancel()
+
+	err := m.DB.QueryRow(ctx, query, id).Scan(
+		&book.ID,
+		&book.CreatedAt,
+		&book.Title,
+		&book.Sales,
+		&book.Pages,
+		&book.Year,
+		&book.Runtime,
+		&book.Genres,
+		&book.Version,
+	)
+	// Handle any errors. If there was no matching book found, Scan() will return
+	// a sql.ErrNoRows error. We check for this and return our custom ErrRecordNotFound
+	// error instead.
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	// Otherwise, return a pointer to the Books struct.
+	return &book, nil
+}
+
+func (m BookModel) Update(book *Books) error {
+	// Add the 'AND version = $6' clause to the SQL query.
+	query := `
+SELECT id, created_at, title, sales, pages, year, runtime, genres, version
+FROM books
+WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+AND (genres @> $2 OR $2 = '{}')
+ORDER BY id`
+
+	args := []any{
+		book.Title,
+		book.Year,
+		book.Sales,
+		book.Pages,
+		book.Runtime,
+		book.Genres,
+		book.ID,
+		book.Version, // Add the expected book version.
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// Execute the SQL query. If no matching row could be found, we know the book
+	// version has changed (or the record has been deleted) and we return our custom
+	// ErrEditConflict error.
+	err := m.DB.QueryRow(ctx, query, args...).Scan(&book.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (m BookModel) Delete(id int64) error {
+	// Return an ErrRecordNotFound error if the movie ID is less than 1.
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+	// Construct the SQL query to delete the record.
+	query := `
+DELETE FROM books
+WHERE id = $1`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	// Execute the SQL query using the Exec() method, passing in the id variable as
+	// the value for the placeholder parameter. The Exec() method returns a sql.Result
+	// object.
+	result, err := m.DB.Exec(ctx, query, id)
+	if err != nil {
+		return err
+	}
+	// Call the RowsAffected() method on the sql.Result object to get the number of rows
+	// affected by the query.
+	rowsAffected := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	// If no rows were affected, we know that the movies table didn't contain a record
+	// with the provided ID at the moment we tried to delete it. In that case we
+	// return an ErrRecordNotFound error.
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+	return nil
+}
+
+func (m BookModel) GetAll(title string, sales int32, pages int32, genres []string, filters Filters) ([]*Books, Metadata, error) {
+	// Construct the SQL query to retrieve all movie records.
+	query := fmt.Sprintf(`
+SELECT count(*) OVER(), id, created_at, title, sales, pages, year, runtime, genres, version
+FROM books
+WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+AND (genres @> $2 OR $2 = '{}')
+ORDER BY %s %s, id ASC
+LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection())
+
+	// Create a context with a 3-second timeout.
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	args := []any{title, sales, pages, genres, filters.limit(), filters.offset()}
+	// Use QueryContext() to execute the query. This returns a sql.Rows resultset
+	// containing the result.
+	rows, err := m.DB.Query(ctx, query, args...)
+	if err != nil {
+		return nil, Metadata{}, err
+	}
+
+	defer rows.Close()
+	// Initialize an empty slice to hold the movie data.
+	books := []*Books{}
+	totalRecords := 0
+	// Use rows.Next to iterate through the rows in the resultset.
+	for rows.Next() {
+		// Initialize an empty Books struct to hold the data for an individual book.
+		var book Books
+		// Scan the values from the row into the Books struct. Again, note that we're
+		// using the pq.Array() adapter on the genres field here.
+		err := rows.Scan(
+			&totalRecords,
+			&book.ID,
+			&book.CreatedAt,
+			&book.Title,
+			&book.Sales,
+			&book.Pages,
+			&book.Year,
+			&book.Runtime,
+			&book.Genres,
+			&book.Version,
+		)
+		if err != nil {
+			return nil, Metadata{}, err
+		}
+		// Add the Books struct to the slice.
+		books = append(books, &book)
+	}
+	// When the rows.Next() loop has finished, call rows.Err() to retrieve any error
+	// that was encountered during the iteration.
+	if err = rows.Err(); err != nil {
+		return nil, Metadata{}, err
+	}
+
+	metadata := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
+	// If everything went OK, then return the slice of books.
+	return books, metadata, nil
+
+}
